0d321fe4deae4079fb07d995389560fca4ff7132
Thapelo Morudu <moruduthapelo56@gmail.com>
2025-08-28 21:13:22 +0200

    Added a hover effect to the nav iterms for improved user experience

--- recipe-sharing-app/src/components/navBar.jsx
+++ recipe-sharing-app/src/components/navBar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div className='bg-gray-300 rponded-lg h-10 font-bold'>
+        <div className='bg-gray-300 rounded-lg h-10 font-bold'>
           <ul  className='flex gap-4 justify-end'> 
-            <li className="hover:underline"><Link to="/">Home</Link></li>
-            <li  className="hover:underline"><Link to="/Recipes">Recipes</Link></li>
-            <li  className="hover:underline"><Link to="/About">About</Link></li>
-            <li  className="hover:underline"><Link to="/Contacts">Contacts</Link></li>
+            <li className="hover:underline hover:bg-gray-400 rounded-md"><Link to="/">Home</Link></li>
+            <li  className="hover:underline hover:bg-gray-400 rounded-md"><Link to="/Recipes">Recipes</Link></li>
+            <li  className="hover:underline hover:bg-gray-400 rounded-md"><Link to="/About">About</Link></li>
+            <li  className="hover:underline hover:bg-gray-400 rounded-md"><Link to="/Contacts">Contacts</Link></li>
           </ul>
         </div>
     );
