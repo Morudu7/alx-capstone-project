0175e4827e04082ce7036c1dd6e868004a613376
Thapelo Morudu <moruduthapelo56@gmail.com>
2025-08-29 20:54:38 +0200

    Adding links to the icons

--- recipe-sharing-app/src/components/Footer.jsx
+++ recipe-sharing-app/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaYoutube, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 
 const Footer = () => {
@@ -9,9 +9,9 @@ const Footer = () => {
         <p>&copy; HALOfood 2025. All rights reserved.</p>
 
         <div className='flex gap-4 justify-end'>
-            <FaFacebook size={30} color="#3b5998" />
-            <FaTwitter size={30} color="#1DA1F2" />
-            <FaInstagram size={30} color="#E1306C" />
+           <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer"><FaYoutube size={30} color="#cd1111ff"/></a>
+            <a href="https://x.com/" target="_blank" rel="noopener noreferrer"><FaTwitter size={30} color="#1DA1F2" /></a>
+           <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagram size={30} color="#E1306C" /> </a>
         </div>
       </footer>
     </div>
